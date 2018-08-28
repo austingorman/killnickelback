0d8fef03cb1c0fdc955cc9f28af9f992d4f5571f
Austin Gorman <austin.gorman@gmail.com>
2018-08-28 13:48:27 -0500

    finished exercise

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nickelback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string, string)> GoodSongs = new List<(string, string)>();
+            HashSet<(string artist, string song)> allSongs = new HashSet<(string, string)>()
+            {
+                ("Nickelback", "How You Remind Me"),
+                ("Nickelback", "Photograph"),
+                ("Nickelback", "Rockstar"),
+                ("Black Moth Super Rainbow", "Twin of Myself"),
+                ("Khruangbin", "Rules"),
+                ("Skatt Bros", "Walk The Night"),
+                ("Jon Hopkins", "Emerald Rush")
+            };
+
+            foreach (var song in allSongs)
+            {
+                if (song.artist != "Nickelback")
+                {
+                    GoodSongs.Add(song);
+                }
+            }
+            foreach (var song in GoodSongs)
+            {
+                Console.WriteLine(song);
+            }
+        }
+    }
+}
